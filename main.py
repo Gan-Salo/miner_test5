eb2c15412590ff962e173ebb994b44078d7e8592
Георгий <ovsyannikov.gosha@list.ru>
2024-05-05 15:05:55 +0700

    Добавлена инициализация размеров поля и количества мин

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from tkinter import messagebox
 
 
 class Miner:
-    def __init__(self):
-        pass
+    def __init__(self, rows=10, cols=10, mines=20):
+        self.rows = rows
+        self.cols = cols
+        self.mines = mines
--- tests.py
+++ tests.py
@@ -7,6 +7,12 @@ class TestMinesweeper(unittest.TestCase):
     def testMinerClassCreation(self):
         miner = Miner()
         self.assertIsNotNone(miner)
+    def test_initial_attributes(self):
+        rows, cols, mines = 10, 10, 20
+        miner = Miner(rows, cols, mines)
+        self.assertEqual(miner.rows, rows)
+        self.assertEqual(miner.cols, cols)
+        self.assertEqual(miner.mines, mines)
 
 if __name__ == '__main__':
     unittest.main()
