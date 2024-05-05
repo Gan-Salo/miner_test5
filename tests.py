bcad53ad1f0e9821297eb6f8a0371b76a5a9f97d
Георгий <ovsyannikov.gosha@list.ru>
2024-05-05 15:34:36 +0700

    Добавлен тест на расстановку мин при инициализации. Тест не пройден.

--- tests.py
+++ tests.py
@@ -19,5 +19,11 @@ class TestMinesweeper(unittest.TestCase):
         miner = Miner(rows, cols, mines)
         self.assertEqual(sum(row.count('M') for row in miner.board), 0)
 
+    def test_mines_placement(self):
+        rows, cols, mines = 10, 10, 20
+        miner = Miner(rows, cols, mines)
+        actual_mines_count = sum(row.count('M') for row in miner.board)
+        self.assertEqual(actual_mines_count, mines, "Количество расставленных мин не совпадает с инициализированным количеством.")
+
 if __name__ == '__main__':
     unittest.main()
